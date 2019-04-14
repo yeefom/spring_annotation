d7fc43bb8fb197f74230a06ec545710cfadc869c
Yifeng Wang <yifeng@yeefom.com>
2019-04-13 21:46:44 -0700

    add some bean life cycle methods

--- src/main/java/annotation/TennisCoach.java
+++ src/main/java/annotation/TennisCoach.java
@@ -1,11 +1,13 @@
 package annotation;
 
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-public class TennisCoach implements Coach {
+public class TennisCoach implements Coach, InitializingBean, DisposableBean {
 
     @Autowired
     @Qualifier("otherEquipmentService")
@@ -20,6 +22,16 @@ public class TennisCoach implements Coach {
     // public void setEquipmentService(EquipmentService equipmentService) {
     //     this.equipmentService = equipmentService;
     // }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("post construct tennis coach");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("before destroy tennis coach");
+    }
 
     @Override
     public String getWorkout() {
